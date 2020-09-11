756feddb0b07f85bce856d3a50eb3fa1a9c93461
Mickael <mickael.lehoux@greenponik.com>
2020-09-11 10:27:43 +0200

    edit docs

--- docs/html/search/searchdata.js
+++ docs/html/search/searchdata.js
@@ -1,27 +1,18 @@
 var indexSectionsWithContent =
 {
-  0: "_aceghlpqrsw",
-  1: "acep",
-  2: "g",
-  3: "_ghlqrsw",
-  4: "r"
+  0: "r",
+  1: "r"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "classes",
-  2: "namespaces",
-  3: "functions",
-  4: "pages"
+  1: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
-  1: "Classes",
-  2: "Namespaces",
-  3: "Functions",
-  4: "Pages"
+  1: "Pages"
 };
 
