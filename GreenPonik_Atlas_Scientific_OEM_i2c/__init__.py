01f6ea171b298f63805ff64df619306ff85efb3e
Mickael <mickael.lehoux@greenponik.com>
2021-11-15 14:39:21 +0100

    fix doc

--- GreenPonik_Atlas_Scientific_OEM_i2c/__init__.py
+++
@@ -1,9 +0,0 @@
-# from GreenPonik_Atlas_Scientific_OEM_i2c.GreenPonik_AtlasOEMI2c import (
-#     AtlasOEMI2c
-# )
-# from GreenPonik_Atlas_Scientific_OEM_i2c.GreenPonik_ECI2c import (
-#     ECI2c
-# )
-# from GreenPonik_Atlas_Scientific_OEM_i2c.GreenPonik_PHI2c import (
-#     PHI2c
-# )
