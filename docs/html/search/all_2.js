5c30c2ee8bf657f6c3a7789ad701d201b4e765a8
Mickael <mickael.lehoux@greenponik.com>
2021-11-15 12:48:25 +0100

    convert documentation from doxygen to sphinx

--- docs/html/search/all_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['commonsi2c_2',['CommonsI2c',['../class_green_ponik___atlas___scientific__i2c_1_1_commons_i2c.html',1,'GreenPonik_Atlas_Scientific_i2c']]]
-];
--- docs/html/search/namespaces_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['greenponik_5fatlas_5fscientific_5fi2c_42',['GreenPonik_Atlas_Scientific_i2c',['../namespace_green_ponik___atlas___scientific__i2c.html',1,'']]]
-];
